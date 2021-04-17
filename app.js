fd3ba6c644822b9ffe68f6b9adc4d2fa71e630f2
Kemal Aydık <kemalaydik@gmail.com>
2021-04-17 21:32:34 +0300

    -

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
-d3.select('#new-note')
-	.on('submit', function () {
-		d3.event.preventDefault();
-		var input = d3.select('input');
-		d3.select('#notes')
-			.append('p')
-			.classed('note', true)
-			.text(input.property('value'));
-		input.property('value', '');
-	})
+var quotes = [
+	{
+		quote: 'I see dead people.',
+		movie: 'The Sixth Sense',
+		year: 1999,
+		rating: 'PG-13',
+	},
+	{
+		quote: 'May the force be with you.',
+		movie: 'Star Wars: Episode IV - A New Hope',
+		year: 1977,
+		rating: 'PG',
+	},
+	{
+		quote:
+			"You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?",
+		movie: 'Dirty Harry',
+		year: 1971,
+		rating: 'R',
+	},
+	{
+		quote: "You had me at 'hello.'",
+		movie: 'Jerry Maguire',
+		year: 1996,
+		rating: 'R',
+	},
+	{
+		quote:
+			'Just keep swimming. Just keep swimming. Swimming, swimming, swiming.',
+		movie: 'Finding Nemo',
+		year: 2003,
+		rating: 'G',
+	},
+];
 
-  d3.select('.remove')
-  .on('click',()=>d3.selectAll('p').remove())
+a=d3.select('#quotes')
+	.style('list-style', 'none')
+	.selectAll('li')
+	.data(quotes)
+	.enter()
+	.append('li')
+  .text(d=>d.quote+d.movie+d.year)
+  .style('margin',"20px")
+  // .selectAll('li')
+  // .data(quotes)
+  .exit()
+
+  console.log(a);
