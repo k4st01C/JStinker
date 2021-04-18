2783c2ba4c8241415d8728b00854921ffa27d5ee
Kemal Aydık <kemalaydik@gmail.com>
2021-04-18 03:29:23 +0300

    -

--- app.js
+++ app.js
@@ -1,48 +1,31 @@
-var quotes = [
-	{
-		quote: 'I see dead people.',
-		movie: 'The Sixth Sense',
-		year: 1999,
-		rating: 'PG-13',
-	},
-	{
-		quote: 'May the force be with you.',
-		movie: 'Star Wars: Episode IV - A New Hope',
-		year: 1977,
-		rating: 'PG',
-	},
-	{
-		quote:
-			"You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?",
-		movie: 'Dirty Harry',
-		year: 1971,
-		rating: 'R',
-	},
-	{
-		quote: "You had me at 'hello.'",
-		movie: 'Jerry Maguire',
-		year: 1996,
-		rating: 'R',
-	},
-	{
-		quote:
-			'Just keep swimming. Just keep swimming. Swimming, swimming, swiming.',
-		movie: 'Finding Nemo',
-		year: 2003,
-		rating: 'G',
-	},
-];
+// write your code here!
 
-a=d3.select('#quotes')
-	.style('list-style', 'none')
-	.selectAll('li')
-	.data(quotes)
-	.enter()
-	.append('li')
-  .text(d=>d.quote+d.movie+d.year)
-  .style('margin',"20px")
-  // .selectAll('li')
-  // .data(quotes)
-  .exit()
+const getFrequencies = (str) => {
+	let occObj = str
+		.split('')
+		.sort()
+		.reduce((a, e) => {
+			return a[e] ? a[e]++ : (a[e] = 1), a;
+		}, {});
+	return Object.keys(occObj).map((e) => {
+		const val = occObj[e];
+		return { character: e, count: val };
+	});
+};
 
-  console.log(a);
+d3.select('form').on('submit', () => {
+	d3.event.preventDefault();
+	let strFrequencies = getFrequencies(d3.select('input').property('value'));
+	console.log(strFrequencies);
+	d3.select('#letters')
+    .selectAll('.letter')
+		.data(strFrequencies)
+		.enter()
+		.append('div')
+		.text((d) => d.character)
+        .classed('letter',true)
+        .style('height',d=>d.count*20+'px')
+        .style('width','20px')
+        .style('line-height','20px')
+        .style('margin-right','5px')
+});
