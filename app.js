7512a515aa12d96702d5eb911714fca199fb547f
Kemal Aydık <kemalaydik@gmail.com>
2021-04-19 21:57:44 +0300

    -

--- app.js
+++ app.js
@@ -1,35 +1,79 @@
-const data = [3, 2, 4, 6, 7];
-const width = 600;
-const height = 600;
-const r = 50;
+window.addEventListener('DOMContentLoaded', (event) => {
+	// document.querySelector('select').innerHTML=    '<option value="volvo">Volvo</option>'
+});
 
-function plot(svg, data) {
-	d3.select(svg)
-		.attr('width', width)
-		.attr('height', height)
-		.selectAll('circle')
-		.data(data)
-		.enter()
-		.append('circle')
-		.attr('cx', (d, i) => i * r * 2 + r)
-		.attr('cy', height / 2)
-		.attr('r', (d) => d*10)
-		.attr('fill', (d) => `hsl(120,100%,${d*10}%`)
-        .on("click", function(e, i){
-            data.splice(i, 1);
-            plot();
-          })
-		.exit()
-		.remove()
-}
+const w = 500,
+	h = 500,
+	padding = 30;
 
-function remove(){
-    data.pop()
-    plot('svg', data);
-}
+const yScale = d3
+	.scaleLinear()
+	.domain(d3.extent(birthData2011, (d) => d.lifeExpectancy))
+	.range([h - padding, padding]);
+const xScale = d3
+	.scaleLinear()
+	.domain(d3.extent(birthData2011, (d) => d.births / d.population))
+	.range([padding, w - padding]);
 
-window.addEventListener('DOMContentLoaded', () => {
-	plot('svg', data);
-})
+const colorScale = d3
+	.scaleLinear()
+	.domain(d3.extent(birthData2011, (d) => d.population / d.area))
+	.range(['lightgreen', 'black']);
 
-document.querySelectorAll('circle')
+const radiusScale = d3
+	.scaleLinear()
+	.domain(d3.extent(birthData2011, (d) => d.births))
+	.range([2, 40]);
+
+const xAxis = d3.axisBottom(xScale).tickSize(-h+2*padding)
+const yAxis = d3.axisLeft(yScale).tickSize(-w+2*padding)
+
+d3.select('svg').append('g')
+.attr('transform',`translate(0,${h-padding})`)
+.call(xAxis);
+
+d3.select('svg').append('g')
+.attr('transform',`translate(${padding},0)`)
+.call(yAxis);
+
+
+
+d3.select('svg')
+	.attr('width', w)
+	.attr('height', h)
+	.selectAll('circle')
+	.data(birthData2011)
+	.enter()
+	.append('circle')
+	.attr('cx', (d) => xScale(d.births / d.population))
+	.attr('cy', (d) => yScale(d.lifeExpectancy))
+	.attr('r', (d) => radiusScale(d.births))
+	.attr('fill', (d) => colorScale(d.population / d.area))
+	.attr('stroke-width', 1)
+	.attr('stroke', 'blue');
+
+
+d3.select('svg')
+.append('text')
+.attr('x',w/2)
+.attr('y',h-padding)
+.attr('dy','1.5em')
+.style('text-anchor','middle')
+.text('Births per Capita')
+
+d3.select('svg')
+.append('text')
+.attr('x',w/2)
+.attr('y',padding)
+.attr('font-size','1.5em')
+.style('text-anchor','middle')
+.text('Data on Births by Country in 2011')
+
+d3.select('svg')
+.append('text')
+.attr('transform','rotate(-90)')
+.attr('x',-h/2)
+.attr('y',-5)
+.attr('dy','1.1em')
+.style('text-anchor','middle')
+.text('Life Expectancy')
