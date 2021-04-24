963b274d518ea08bce618197fee8d37119bdafcb
Kemal Aydık <kemalaydik@gmail.com>
2021-04-24 23:58:21 +0300

    -

--- app.js
+++ app.js
@@ -1,55 +1,25 @@
-let ctx = document.getElementById('monthlySales').getContext('2d');
-let pieCtx = document.getElementById('deptSales').getContext('2d');
+function myIterator(start,finish) {
+	let idx=start;
+	let count=0;
+	return {
+		next() {
+			let result;
+			if (idx<finish) {
+				result={value:idx,done:false};
+				idx+=1;
+				count++
+				return result;
+			}
+			return {value:count, done:true}
+		}
+	}
 
-let a=Array.of(12,6,1)
-const data = {
-	labels: ['Oct', 'Nov', 'Dec'],
-	datasets: [
-		{
-			label: '# of Sales',
-			data: [12, 9, 13],
-			backgroundColor: [
-				'rgba(238,183,104,1)',
-				'rgba(75,166,223,1)',
-				'rgba(239,118,122,1)',
-			],
-			borderWidth: 0,
-		},
-	],
-};
+}
 
-let monthlySales = new Chart(ctx, {
-	type: 'bar',
-	data,
-	options: {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: 'top',
-			},
-			title: {
-				display: true,
-				text: 'Chart.js Bar Chart',
-			},
-		},
-	},
-});
+const it=myIterator(0,10)
+let res=it.next
 
-let deptSales = new Chart(pieCtx, {
-	type: 'pie',
-	data: {
-		labels: ['Hiking', 'Running', 'Swimming'],
-		datasets: [
-			{
-				label: '# of Sales',
-				data: [12, 9, 13],
-				backgroundColor: [
-					'rgba(238,183,104,1)',
-					'rgba(75,166,223,1)',
-					'rgba(239,118,122,1)',
-				],
-				borderWidth: 0,
-			},
-		],
-	},
-});
+while (!res.done) {
+	console.log(res.value);
+	res=it.next()
+}
