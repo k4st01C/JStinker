7c064b284db8eac04d30f81066468ce5f1e33c68
Kemal Aydık <kemalaydik@gmail.com>
2022-03-20 01:28:59 +0300

    .

--- Tinker/index.js
+++ Tinker/index.js
@@ -1,55 +1,14 @@
-const API_KEY = '9e37f878d5870217dced3e100a27f300';
-const imgUrl = 'https://image.tmdb.org/t/p/';
-const imgSize = 'w500';
+const btns = document.querySelectorAll('.arrow');
+const slides = document.querySelectorAll('.slide');
+const body = document.body;
 
-const main = document.querySelector('main');
-const form = document.querySelector('form');
-const input = document.querySelector('form>input');
+let activeSlide = 0;
 
-const parameters = {
-	params: {
-		api_key: API_KEY,
-		language: 'en-US',
-		query: input.value || '',
-	},
-};
+btns.forEach(e => e.addEventListener('click', paginate));
 
-axios.get('https://api.themoviedb.org/3/movie/popular', parameters).then(renderMovies);
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	axios
-		.get('https://api.themoviedb.org/3/search/movie', {
-			params: {
-				api_key: API_KEY,
-				language: 'en-US',
-				query: input.value,
-			},
-		})
-		.then(renderMovies);
-});
-
-function renderMovies(resp) {
-	const html = resp.data.results.reduce((ac, movie) => {
-		return (
-			ac +
-			`<div class="movie">
-	          <img src="${imgUrl + imgSize + movie.poster_path}" alt="">
-	          <div class="movie-info">
-	              <h3 class="movie-title">${movie.original_title}</h3>
-	              <span class="${colorRate(movie)}">${movie.vote_average}</span>
-	          </div>
-	          <div class="overview">
-	              <h3>Overview</h3>
-	              ${movie.overview}
-	          </div>
-      </div>`
-		);
-	}, '');
-	main.innerHTML = html;
-}
-
-function colorRate(movie) {
-	if (movie.vote_average >= 8) return 'green';
-	if (movie.vote_average >= 7) return 'orange';
-	return 'red';
+function paginate() {
+	slides[activeSlide].classList.remove('active');
+	activeSlide = this.classList.contains('left') ? (--activeSlide + 5) % 5 : (--activeSlide + 5) % 5;
+	slides[activeSlide].classList.add('active');
+	body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
 }
