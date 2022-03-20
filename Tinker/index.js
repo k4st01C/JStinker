3cbf1a0ff6b0e089914e483e058da956c8ad67b0
Kemal Aydık <kemalaydik@gmail.com>
2022-03-20 21:18:29 +0300

    .

--- Tinker/index.js
+++ Tinker/index.js
@@ -1,14 +1,41 @@
-const btns = document.querySelectorAll('.arrow');
-const slides = document.querySelectorAll('.slide');
-const body = document.body;
+const html = document.querySelector('html');
+const toggle = document.querySelector('.toggle');
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
 
-let activeSlide = 0;
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-btns.forEach(e => e.addEventListener('click', paginate));
+toggle.addEventListener('click', () => {
+	toggle.innerText = toggle.innerText === 'Dark Mode' ? 'Light Mode' : 'Dark Mode';
+	html.classList.toggle('dark');
+});
 
-function paginate() {
-	slides[activeSlide].classList.remove('active');
-	activeSlide = this.classList.contains('left') ? (--activeSlide + 5) % 5 : (--activeSlide + 5) % 5;
-	slides[activeSlide].classList.add('active');
-	body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+const now = new Date();
+let minute = now.getMinutes() * 6;
+let hour = ((now.getHours() % 12) + minute / 360) * 30;
+let second = now.getSeconds() * 6;
+let time = now.getDay();
+let month = now.getMonth();
+let date = now.getDate();
+
+function pad(num) {
+	if (num < 10) return '0' + num;
+	return num;
 }
+
+function setTime() {
+	secondEl.style.transform = `translate(-50%, -100%) rotate(${second}deg)`;
+	minuteEl.style.transform = `translate(-50%, -100%) rotate(${minute}deg)`;
+	hourEl.style.transform = `translate(-50%, -100%) rotate(${hour}deg)`;
+	second = (second + 6) % 360;
+	minute = (minute + 1 / 60) % 360;
+	hour = (hour + 1 / 120) % 360;
+	timeEl.innerText = pad(~~(hour / 30)) + ':' + pad(~~(minute / 6));
+	dateEl.innerHTML = `${days[time]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+setInterval(setTime, 1000);
