01ae4e191752f8bc5d4103ecbca8c37dd9f9b522
Kemal Aydık <kemalaydik@gmail.com>
2022-03-21 18:33:20 +0300

    .

--- Tinker/index.js
+++ Tinker/index.js
@@ -1,12 +1,69 @@
-const btn = document.querySelector('.btn');
+const canvas = document.querySelector('canvas');
+const decreaseEl = document.getElementById('decrease');
+const increaseEl = document.getElementById('increase');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+const ctx = canvas.getContext('2d');
 
-btn.addEventListener('click', e => {
-	const btnTop = btn.offsetTop;
-	const btnLeft = btn.offsetLeft;
-	const ripple = document.createElement('span');
-	ripple.classList.add('circle');
-	ripple.style.top = e.clientY - btnTop + 'px';
-	ripple.style.left = e.clientX - btnLeft + 'px';
-	btn.appendChild(ripple);
-	setTimeout(() => ripple.remove(), 500);
+let x,
+	y,
+	color = 'black',
+	isPressed = false,
+	size = 20;
+
+function drawCircle(x, y) {
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = size * 2;
+	ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', e => {
+	isPressed = true;
+	x = e.offsetX;
+	y = e.offsetY;
+});
+canvas.addEventListener('mouseup', e => {
+	isPressed = false;
+	x = undefined;
+	y = undefined;
+});
+
+canvas.addEventListener('mousemove', e => {
+	if (isPressed) {
+		const x2 = e.offsetX;
+		const y2 = e.offsetY;
+		drawCircle(x2, y2);
+		drawLine(x, y, x2, y2);
+		x = x2;
+		y = y2;
+	}
+});
+
+decreaseEl.addEventListener('click', () => {
+	--size;
+	sizeEl.innerText = size;
+});
+
+increaseEl.addEventListener('click', () => {
+	++size;
+	sizeEl.innerText = size;
+});
+
+colorEl.addEventListener('change', () => {
+	color = colorEl.value;
+});
+
+clearEl.addEventListener('click', () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
