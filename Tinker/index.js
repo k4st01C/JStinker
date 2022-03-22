cfecb64b54377d5e31c8282ecb893c11d7d72194
Kemal Aydık <kemalaydik@gmail.com>
2022-03-22 22:41:26 +0300

    .

--- Tinker/index.js
+++ Tinker/index.js
@@ -1,10 +1,35 @@
-const nav = document.querySelector('nav');
-document.addEventListener('scroll', myFunction);
+const btns = document.querySelectorAll('button');
 
-function myFunction() {
-	if (window.scrollY > 250) {
-		document.getElementById('nav').classList.add('styled');
-	} else {
-		document.getElementById('nav').classList.remove('styled');
-	}
-}
+btns[0].addEventListener('click', () => {
+	let limit = Math.max(
+		document.body.scrollHeight,
+		document.body.offsetHeight,
+		document.documentElement.clientHeight,
+		document.documentElement.scrollHeight,
+		document.documentElement.offsetHeight,
+	);
+
+	const Y = window.pageYOffset + document.documentElement.clientHeight;
+	console.log(Y, limit);
+	window.scroll({
+		left: 0,
+		top: Y === limit ? 0 : Y,
+		behavior: 'smooth',
+	});
+});
+btns[1].addEventListener('click', () => {
+	let limit = Math.max(
+		document.body.scrollHeight,
+		document.body.offsetHeight,
+		document.documentElement.clientHeight,
+		document.documentElement.scrollHeight,
+		document.documentElement.offsetHeight,
+	);
+	const Y = window.pageYOffset - document.documentElement.clientHeight;
+	console.log(Y);
+	window.scroll({
+		left: 0,
+		top: window.pageYOffset === 0 ? limit : Y,
+		behavior: 'smooth',
+	});
+});
