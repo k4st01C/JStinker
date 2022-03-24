16954ca1562f9038c861d2dd9aada0d3e50be3e8
Kemal Aydık <kemalaydik@gmail.com>
2022-03-24 22:07:31 +0300

    .

--- Tinker/index.js
+++ Tinker/index.js
@@ -1,18 +1,64 @@
-const btnNotification = document.querySelector('.btn-notification');
-const notificationContainer = document.querySelector('.notification-container');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const img = document.querySelector('img');
+const name = document.querySelector('.name');
+const bio = document.querySelector('.bio');
+const followers = document.querySelector('.followers');
+const following = document.querySelector('.following');
+const repos = document.querySelector('.repos');
 
-btnNotification.addEventListener('click', notify);
+const url = 'https://api.github.com/users/';
 
-const messages = ['message 1', 'message 2', 'message3'];
-notificationIdx = 0;
+function listRepos(repos) {
+	return repos.reduce((ac, repo) => {
+		ac += `<small>${repo.name}</small>`;
+		return ac;
+	}, '');
+}
 
-function notify() {
-	notificationIdx = ++notificationIdx % 3;
-	const notification = document.createElement('button');
-	notification.innerText = messages[notificationIdx];
-	if (notificationIdx === 0) notification.classList.add('green');
-	else if (notificationIdx === 1) notification.classList.add('yellow');
-	else if (notificationIdx === 2) notification.classList.add('red');
-	notificationContainer.append(notification);
-	setTimeout(() => notification.remove(), 2000);
+function getURL(url) {
+	return axios.get(url);
+}
+
+async function fetchRepos() {}
+
+function render(data, repos) {
+	const { avatar_url, bio, name, followers, following, public_repos } = data;
+	return `<div class="user-card">
+        <img class="profile" src=${avatar_url} alt="">
+        <div class="text">
+            <h2 class="name">${name}</h2>
+            <p class="bio">${bio}</p>
+            <div class="metrics">
+                <h5 class="followers">${followers} Followers</h5>
+                <h5 class="following">${following} Following</h5>
+                <h5 class="repo">${public_repos} Repos</h5>
+            </div>
+            <div class="repos">
+            ${listRepos(repos)}
+            </div>
+        </div>
+    </div>`;
+}
+
+form.addEventListener('submit', fetchUrl);
+
+async function fetchUrl(event) {
+	event.preventDefault();
+	try {
+		const [
+			{
+				value: { data },
+			},
+			{
+				value: {
+					data: [...repos],
+				},
+			},
+		] = await Promise.allSettled([getURL(url + input.value), getURL(url + input.value + '/repos')]);
+		form.insertAdjacentHTML('afterend', render(data, repos));
+	} catch (error) {
+		console.log(error);
+		form.insertAdjacentHTML('afterend', error);
+	}
 }
