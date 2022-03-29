7b4dcbc14c87fce035e4c6ce5eacc68d3af41300
Kemal Aydık <kemalaydik@gmail.com>
2022-03-29 17:50:55 +0300

    1.1

--- Tinker/index.js
+++ Tinker/index.js
@@ -1,50 +1,15 @@
-const URL = 'https://pokeapi.co/api/v2/pokemon/';
-const CARD_NUMBER = 150;
+const img = document.querySelector('.img');
 
-const container = document.querySelector('.container');
+const imgUrls = {
+	fa_home: 100,
+	fa_box: 200,
+	fa_book_open: 300,
+	fa_users: 400,
+};
 
-const promiseArray = [];
-
-function renderHTML(img, id, title, cat) {
-	const type = {
-		grass: 'lightgreen',
-		fire: 'orange',
-		water: 'lightblue',
-		bug: 'yellow',
-		normal: '#eee',
-		poison: 'lightcoral',
-		electric: 'cyan',
-	};
-
-	return `
-	        <div class="card" style="background-color:${type[cat]}">
-            <img src="${img}" alt="">
-            <small>#00${id}</small>
-            <h2>${title}</h2>
-            <p>Type: ${cat}</p>
-        </div>
-	`;
-}
-
-for (let i = 1; i <= CARD_NUMBER; i++) promiseArray.push(axios.get(URL + i));
-
-async function fetchPokemons() {
-	const data = await Promise.all(promiseArray);
-	for (let i = 0; i < CARD_NUMBER; i++) {
-		const {
-			data: {
-				name: title,
-				id,
-				sprites: { front_default: img },
-				types: [
-					{
-						type: { name: type },
-					},
-				],
-			},
-		} = data[i];
-		container.insertAdjacentHTML('beforeend', renderHTML(img, id, title, type));
+document.addEventListener('click', e => {
+	if (e.target.classList.contains('fa-solid')) {
+		const temp = e.target.classList[1].replaceAll('-', '_');
+		img.style.backgroundImage = `url("https://picsum.photos/${imgUrls[temp]}`;
 	}
-}
-
-fetchPokemons();
+});
