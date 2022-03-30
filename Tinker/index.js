6861933346530d89d6d009df1a0e96cf56783e2d
Kemal Aydık <kemalaydik@gmail.com>
2022-03-30 23:43:33 +0300

    .

--- Tinker/index.js
+++ Tinker/index.js
@@ -1,13 +1,17 @@
-const boxes = document.querySelector('.boxes');
-const btnMagic = document.querySelector('.magic');
+const numbers = document.querySelectorAll('input');
 
-for (let i = 0; i < 4; i++) {
-	for (let j = 0; j < 4; j++) {
-		const div = document.createElement('div');
-		div.className = 'box';
-		div.style.backgroundPosition = `${j * -125}px ${i * -125}px`;
-		boxes.append(div);
-	}
-}
-
-btnMagic.addEventListener('click', () => boxes.classList.toggle('big'));
+numbers.forEach((e, i) =>
+	e.addEventListener('keydown', el => {
+		if (el.key === 'Backspace') {
+			if (i === 0) return;
+			e.previousElementSibling.focus();
+			e.classList.remove('active');
+			e.previousElementSibling.classList.add('active');
+		} else {
+			if (i === 5) return;
+			e.nextElementSibling.focus();
+			e.classList.remove('active');
+			e.nextElementSibling.classList.add('active');
+		}
+	}),
+);
